d2bd26c1f7b91279beab14f3a42926e780c95ca5
kko27 <kwangbemk@gmail.com>
2021-08-26 21:26:59 +0200

    We added 2 new questions

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("How are you doing?")
+    print("where do you live")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
